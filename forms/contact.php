37235f43fe5b9bedd288f9c93040ca4c983e7a22
Sanika Patil <sanikapatil1417@gmail.com>
2026-03-14 12:03:39 +0530

    first commit

--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,13 @@
-<?php
-// Receiver email
-$receiving_email_address = 'sanikapatil1417@gmail.com';
+  <?php
+  // Receiver email
+  $receiving_email_address = 'sanikapatil1417@gmail.com';
 
-// Include the PHP Email Form library
-if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
-  include($php_email_form);
-} else {
-  die('Unable to load the "PHP Email Form" Library!');
-}
+  // Include the PHP Email Form library
+  if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+    include($php_email_form);
+  } else {
+    die('Unable to load the "PHP Email Form" Library!');
+  }
 
 $contact = new PHP_Email_Form;
 $contact->ajax = true;
